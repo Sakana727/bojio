6e1dc1dfe51f5765f91b9cb6650f2973df28c6dd
Sakana727 <22020383@rp.edu.sg>
2024-04-29 15:43:43 +0800

    Auth and Onboarding start

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 // import { authMiddleware } from "@clerk/nextjs/server";
 
 // export default authMiddleware({
-//     publicRoutes: ['/', '/api/webhooks/clerk'],
+//     publicRoutes: ['/', 'c'],
 //     ignoredRoutes: ['/api/webhooks']
 // });
 
@@ -10,34 +10,52 @@
 // };
 
 
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import {
+//   clerkMiddleware,
+//   createRouteMatcher
+// } from '@clerk/nextjs/server';
 
-// // Define routes to be treated differently
-// const publicRoutes = createRouteMatcher(["/", "/api/webhooks/clerk"]);
-// const ignoredRoutes = createRouteMatcher(["/api/webhooks"]);
+// const isProtectedRoute = createRouteMatcher([
+//   '/',
+// ]);
 
 // export default clerkMiddleware((auth, req) => {
-//   if (publicRoutes(req)) {
-//     return;
-//   } else if (ignoredRoutes(req)) {
-//     // No action needed, just let the request pass through
-//   } else {
-//     auth().protect();
-//   }
+//   if (isProtectedRoute(req)) auth().protect();
 // });
 
 // export const config = {
-//   matcher: [
-//     "/((?!.*\\..*|_next).*)",
-//     "/",
-//     "/(api|trpc)(.*)"
-//   ],
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 // };
 
-import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { redirectToSignIn } from "@clerk/nextjs/server";
+import { redirect } from "next/dist/server/api-utils";
+import { NextRequest, NextResponse } from "next/server";
+
+const isOnboardingRoute = createRouteMatcher(["/onboarding"])
+const isPublicRoute = createRouteMatcher(["/sign-in", "/onboarding", "/sign-up","/continue"]);
+
+export default clerkMiddleware((auth, req: NextRequest) => {
+  const { userId, sessionClaims } = auth();
+
+  // For users visiting /onboarding, don't try to redirect
+  if (userId && isOnboardingRoute(req)) {
+    return NextResponse.next();
+  }
+
+  // Catch users who do not have `onboardingComplete: true` in their publicMetadata
+  // Redirect them to the /onboading route to complete onboarding
+  if (userId && !sessionClaims?.metadata?.onboardingComplete) {
+    const onboardingUrl = new URL("/onboarding", req.url);
+    return NextResponse.redirect(onboardingUrl);
+  }
+
+  // If the user is logged in and the route is protected, let them view.
+  if (userId && !isPublicRoute(req)) return NextResponse.next();
+}
+);
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };

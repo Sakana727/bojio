a6d454f25a9aa7de45c9c507eedf568f80dc1f16
Sakana727 <22020383@rp.edu.sg>
2024-05-01 18:58:34 +0800

    Back-end Init with mongoDB & onboarding

--- middleware.ts
+++ middleware.ts
@@ -10,52 +10,53 @@
 // };
 
 
-// import {
-//   clerkMiddleware,
-//   createRouteMatcher
-// } from '@clerk/nextjs/server';
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
 
-// const isProtectedRoute = createRouteMatcher([
-//   '/',
-// ]);
+const isProtectedRoute = createRouteMatcher([
+  '/',
+]);
 
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
-
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { redirectToSignIn } from "@clerk/nextjs/server";
-import { redirect } from "next/dist/server/api-utils";
-import { NextRequest, NextResponse } from "next/server";
-
-const isOnboardingRoute = createRouteMatcher(["/onboarding"])
-const isPublicRoute = createRouteMatcher(["/sign-in", "/onboarding", "/sign-up","/continue"]);
-
-export default clerkMiddleware((auth, req: NextRequest) => {
-  const { userId, sessionClaims } = auth();
-
-  // For users visiting /onboarding, don't try to redirect
-  if (userId && isOnboardingRoute(req)) {
-    return NextResponse.next();
-  }
-
-  // Catch users who do not have `onboardingComplete: true` in their publicMetadata
-  // Redirect them to the /onboading route to complete onboarding
-  if (userId && !sessionClaims?.metadata?.onboardingComplete) {
-    const onboardingUrl = new URL("/onboarding", req.url);
-    return NextResponse.redirect(onboardingUrl);
-  }
-
-  // If the user is logged in and the route is protected, let them view.
-  if (userId && !isPublicRoute(req)) return NextResponse.next();
-}
-);
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { redirectToSignIn } from "@clerk/nextjs/server";
+// import { redirect } from "next/dist/server/api-utils";
+// import { NextRequest, NextResponse } from "next/server";
+
+// const isOnboardingRoute = createRouteMatcher(["/onboarding"])
+// const isPublicRoute = createRouteMatcher(["/sign-in", "/onboarding", "/sign-up","/continue"]);
+
+// export default clerkMiddleware((auth, req: NextRequest) => {
+//   const { userId, sessionClaims } = auth();
+
+//   // For users visiting /onboarding, don't try to redirect
+//   if (userId && isOnboardingRoute(req)) {
+//     return NextResponse.next();
+//   }
+
+
+//   // Catch users who do not have `onboardingComplete: true` in their publicMetadata
+//   // Redirect them to the /onboading route to complete onboarding
+//   if (userId && !sessionClaims?.metadata?.onboardingComplete) {
+//     const onboardingUrl = new URL("/onboarding", req.url);
+//     return NextResponse.redirect(onboardingUrl);
+//   }
+
+//   // If the user is logged in and the route is protected, let them view.
+//   if (userId && !isPublicRoute(req)) return NextResponse.next();
+// }
+// );
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: [
+      "img.clerk.com",
+      "images.clerk.dev",
+      "uploadthing.com",
+      "placehold.co",
+    ],
+  },
+};
+
+module.exports = nextConfig;

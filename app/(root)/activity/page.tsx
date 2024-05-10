6e80dbe8c2cf48c06b8f2c6e09a1ed4c96b0f7f4
Sakana727 <22020383@rp.edu.sg>
2024-05-10 21:21:15 +0800

    Profile Initalisation with displayed posts

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Activity</div>;
+};
+
+export default page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,8 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDatabase } from "../mongoose"
+import Post from "../models/post.model";
+import { model } from "mongoose";
 
 interface Params {
     userId: string,
@@ -58,3 +60,28 @@ export async function fetchUser(userId: string){
   }
 
 }
+
+export async function fetchUserPosts(userId: string){
+  try {
+    connectToDatabase()
+
+    const posts = await User.findOne({ id: userId})
+    .populate({
+      path: 'posts',
+      model: Post,
+      populate: {
+        path: 'children',
+        model: Post,
+        populate: {
+          path: 'author',
+          model: User,
+          select: 'name image id'
+        }
+      }
+    })
+    return posts
+  } catch (error: any){
+      throw new Error(`Failed to fetch user posts: ${error.message}`);
+    }
+    
+  }
